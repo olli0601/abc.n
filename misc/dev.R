4b0f67762961a50bc18beda1ca18282e9b141967
Anton <anton.camacho@lshtm.ac.uk>
2016-01-05 23:02:57 +0100

    update doc

--- misc/dev.R
+++ misc/dev.R
@@ -13,19 +13,26 @@ dev <- function() {
 
 test <- function() {
 
-?ftest.calibrate
+	?ftest.calibrate
 # set number of variables (i.e. summary statistics)
-p <- 3
+	p <- 3
 # set number of observations
-n.of.x <- 100
+	n.of.x <- 100
 # set number of simulations
-n.of.y <- 100
+	n.of.y <- 100
 # set T2 calculated on the observed data
-t2.x <- 0.25
-ftest.calibrate(n.of.x=n.of.x, t2.x=t2.x, p=p, what='KL', mx.pw=0.9, alpha=0.01, use.R= TRUE, debug=TRUE, plot=TRUE, verbose=FALSE)
+	t2.x <- 0.25
+	ftest.calibrate(n.of.x=n.of.x, t2.x=t2.x, p=p, what='KL', mx.pw=0.9, alpha=0.01, use.R= TRUE, debug=TRUE, plot=TRUE, verbose=FALSE)
 
-ftest.calibrate(n.of.x=n.of.x, t2.x=t2.x, p=p, what='KL', mx.pw=0.9, alpha=0.01, use.R= FALSE, plot=FALSE, verbose=FALSE)
+	ftest.calibrate(n.of.x=n.of.x, t2.x=t2.x, p=p, what='KL', mx.pw=0.9, alpha=0.01, use.R= FALSE, plot=FALSE, verbose=FALSE)
 
+}
+
+fix_ratetest <- function() {
+
+	ans <- ratetest.calibrate(n.of.x=30, mean.x=2, n.of.y=30, what='KL', plot=TRUE, debug = TRUE, verbose = TRUE)
+	print(ans)
+	# ratetest.getkl(mean.x = 2, n.of.y=40, tau.u = 2*2, mx.pw = 0.9, alpha = 0.01, pow_scale=1.5, calibrate.tau.u=TRUE, plot=TRUE)
 
 }
 
@@ -33,6 +40,7 @@ main <- function() {
 
 	start_me()
 	dev()
+	fix_ratetest()
 
 }
 
