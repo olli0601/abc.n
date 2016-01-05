c3b2197461cca451f0e4aa12c2d163b769e2e724
Anton <anton.camacho@lshtm.ac.uk>
2016-01-05 23:02:57 +0100

    update doc

--- example/ex.ratetest.calibrate.R
+++ example/ex.ratetest.calibrate.R
@@ -3,14 +3,14 @@
 # this requires to specify c.l, c.u, tau.l, tau.u (ad-hoc ABC parameters), n.of.y, what='ALPHA'
 # note: can be useful to compute the ABC false positive rate for uncalibrated ABC routines
 
-ratetest.calibrate(n.of.y=40, c.l=0.8, c.u=1.2, tau.l=0.5, tau.u=1.5, what='ALPHA', plot=T)
-ratetest.calibrate(n.of.y=40, c.l=0.8, c.u=1.2, tau.l=0.8, tau.u=1.2, what='ALPHA', plot=T)
+ratetest.calibrate(n.of.y=40, c.l=0.8, c.u=1.2, tau.l=0.5, tau.u=1.5, what='ALPHA', plot=TRUE)
+ratetest.calibrate(n.of.y=40, c.l=0.8, c.u=1.2, tau.l=0.8, tau.u=1.2, what='ALPHA', plot=TRUE)
 
 # Example 2: calibrate critical region for given ABC false positive rate and equivalence region
 # this requires to specify alpha (default is 0.01), tau.l, tau.u, n.of.y, what='CR'
 # note: this is just an intermediate calibration and may result in unsuitable power properties
 
-ratetest.calibrate(n.of.y=40, tau.l=1/1.4, tau.u=1.4, alpha=0.01, what='CR', plot=T)
+ratetest.calibrate(n.of.y=40, tau.l=1/1.4, tau.u=1.4, alpha=0.01, what='CR', plot=TRUE)
 
 
 # Example 3: calibrate critical region and power of ABC accept/reject step (default method when not specifying 'what')
@@ -20,7 +20,7 @@ ratetest.calibrate(n.of.y=40, tau.l=1/1.4, tau.u=1.4, alpha=0.01, what='CR', plo
 # the observed summary values. If a set of observed summary values is available,
 # use the calibrations in Example 4.
 
-ratetest.calibrate(n.of.y=40, mx.pw=0.9, tau.u.ub=1.4, what='MXPW', plot=T)
+ratetest.calibrate(n.of.y=40, mx.pw=0.9, tau.u.ub=1.4, what='MXPW', plot=TRUE)
 
 
 # Example 4: calibrate critical region, power of ABC accept/reject step, and number of simulated data points
@@ -29,5 +29,5 @@ ratetest.calibrate(n.of.y=40, mx.pw=0.9, tau.u.ub=1.4, what='MXPW', plot=T)
 # note: the advantage here is that the KL divergence is also minimised, but we also
 # need to have a set of observed summary values
 
-ratetest.calibrate(n.of.x=30, mean.x=2, n.of.y=30, what='KL', plot=T)
+ratetest.calibrate(n.of.x=30, mean.x=2, n.of.y=30, what='KL', plot=TRUE)
 
--- example/ex.ratetest.pow.R
+++ example/ex.ratetest.pow.R
@@ -5,6 +5,8 @@ cali	<- ratetest.calibrate(tau.l=1/2, tau.u=2, n.of.y=n.of.y, what='CR', alpha=0
 # compute the power for the range (0.1, 3)
 rho  	<- seq(0.1, 3, len=1024)
 tmp		<- data.frame(rho=rho, power=ratetest.pow(rho, cali['c.l'], cali['c.u'], m=n.of.y))
+
+library(ggplot2)
 p 		<- ggplot(tmp,aes(x=rho,y=power)) + geom_line() + labs(y='Power\n(ABC acceptance probability)')
 print(p)
 
